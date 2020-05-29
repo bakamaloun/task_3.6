ba3ea26713d32b3650be49d3a9a9ce1c400e1564
owner <bakamaloun@gmail.com>
2020-05-29 14:42:54 +0400

    updated conftest

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None, help="Choose language")
@@ -7,8 +8,11 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option(
+        'prefs', {'intl.accept_languages': language})
     print("\nstart browser with  ", language)
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser")
     browser.quit()
